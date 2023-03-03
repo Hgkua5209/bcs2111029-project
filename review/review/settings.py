1ab8da1339ae6cb99c19bdeeb021a4492c01219c
Taki <126129738+Hgkua5209@users.noreply.github.com>
2023-03-03 11:12:01 +0800

    Update settings.py

--- review/review/settings.py
+++ review/review/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoise.Middleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'review.urls'
